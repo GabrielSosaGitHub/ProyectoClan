18c56f530dea793fb613a02b20fddceda646f34d
Gabriel Sosa <isosa@fi.uba.ar>
2021-12-14 15:20:24 -0300

    Empezando.

---
+++ ProyectoClan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProyectoClan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoClan.settings')
+
+application = get_asgi_application()
